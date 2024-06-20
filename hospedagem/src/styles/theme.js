c82f3ef64817cfa513f78c9561264672b71a4eeb
Felipe Giusti <felipeegiusti@gmail.com>
2024-06-20 10:51:53 -0300

    organização da arquitetura e padrão de temas global do sistema

---
+++ hospedagem/src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#232129", //mais escuro  (fundo)
+    BACKGROUND_800: "#312E38", //médio escuro (campos)
+    BACKGROUND_700: "#3E3B47", //pouco escuro ()
+
+    WHITE: "#F4EDE8",
+
+    GRAY_100: "#999591",
+    GRAY_300: "#666360",
+
+    RED: "#FF002E",
+    GREEN: "#00ff31",
+  }
+}
