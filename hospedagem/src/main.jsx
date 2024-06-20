7d55932feea2313765b9fb96761f9c4bfe49e29f
Felipe Giusti <felipeegiusti@gmail.com>
2024-06-20 11:14:55 -0300

    Configuração básica routes

--- hospedagem/src/main.jsx
+++ hospedagem/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme.js'
 import GlobalStyles from './styles/global.js'
+import { Routes } from './routes/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      {/* <Routes /> */}
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
